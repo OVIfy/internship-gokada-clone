35ea8434497ebcfa62b6bc59e927fc1fe58e9bd7
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2022-09-20 23:52:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var ada = "yeah";
+var girls = ['chinzzy', 'perpetual', 'ngozi'];
+function add(a, b) {
+    return a + b;
+}
+function takeType(x) {
+    return x.a + Number(x.b);
+}
+function printID(x) {
+    console.log(x);
+}
+printID(['yeah', 'ngozi']);
+
+
